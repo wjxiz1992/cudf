2010645c1c2e662d7112ef5efc403199589471ea
Allen Xu <allxu@nvidia.com>
2024-05-14 15:37:08 +0800

    Revert "adapt expermental NVCOMP"
    This reverts commit f9bfb288277ce16965b3c5d6e41d989de9fcfbea.

--- java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
+++ java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
@@ -55,7 +55,7 @@ public class NativeDepsLoader {
    */
   private static final String[][] loadOrder = new String[][]{
       new String[]{
-          "nvcomp_bitcomp", "nvcomp_gdeflate", "nvcomp_optimal_parse"	      
+          "nvcomp_bitcomp", "nvcomp_gdeflate"
       },
       new String[]{
           "nvcomp"
