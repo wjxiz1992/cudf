3df307fbdf4cb5518236de54aa29de6e69840cce
liyuan <yuali@nvidia.com>
2024-09-10 15:14:48 +0800

    cherry pick from branch2410 20240906
    Signed-off-by: liyuan <yuali@nvidia.com>

--- java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
+++ java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
@@ -55,9 +55,6 @@ public class NativeDepsLoader {
    */
   private static final String[][] loadOrder = new String[][]{
       new String[]{
-          "nvcomp_bitcomp", "nvcomp_gdeflate", "nvcomp_optimal_parse"	      
-      },
-      new String[]{
           "nvcomp"
       },
       new String[]{
